beedc9ae62c3403001c94753182520550dc93a9d
NASHE\shote <nachogamallolafon123@gmail.com>
2025-05-05 19:07:42 +0200

    JavaFX , inicio practica 2

---
+++ P2/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module mainfolder.p2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens mainfolder.p2 to javafx.fxml;
+    exports mainfolder.p2;
+}
